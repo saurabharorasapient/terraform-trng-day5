c052f6f99abf59bed0b57ef6a233625515c4fff2
saurabharorasapient <134918136+saurabharorasapient@users.noreply.github.com>
2023-06-02 11:51:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_user" "lb-test" {
-  name = "demo-user1"
-  path = "/system/"
+resource "aws_instance" "ec212" {
+  ami = "ami-0715c1897453cabd1"
+  instance_type = "t2.micro"
 }
